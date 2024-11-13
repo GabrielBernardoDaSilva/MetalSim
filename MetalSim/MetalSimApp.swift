67ad6a48a8ad936fdb51ec1b8eb728cd759a7623
GabrielBernardoDaSilva <gabriel7798@gmail.com>
2024-11-13 15:41:39 -0300

    Initial Commit

---
+++ MetalSim/MetalSimApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalSimApp.swift
+//  MetalSim
+//
+//  Created by Gabriel Bernardo on 13/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MetalSimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MetalSimTests/MetalSimTests.swift
@@ -0,0 +1,17 @@
+//
+//  MetalSimTests.swift
+//  MetalSimTests
+//
+//  Created by Gabriel Bernardo on 13/11/24.
+//
+
+import Testing
+@testable import MetalSim
+
+struct MetalSimTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
