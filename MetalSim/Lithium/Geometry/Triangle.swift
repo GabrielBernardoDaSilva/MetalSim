32bdca538455a67aed68287d57b143290eca32a2
GabrielBernardoDaSilva <gabriel7798@gmail.com>
2024-11-17 00:00:26 -0300

    Split into many files

---
+++ MetalSim/Lithium/Geometry/Triangle.swift
@@ -0,0 +1,8 @@
+//
+//  Triangle.swift
+//  MetalSim
+//
+//  Created by Gabriel Bernardo on 16/11/24.
+//
+
+import Foundation
---
+++ MetalSim/Lithium/LithiumBuffer.swift
@@ -0,0 +1,8 @@
+//
+//  LithiumBuffer.swift
+//  MetalSim
+//
+//  Created by Gabriel Bernardo on 16/11/24.
+//
+
+import Foundation
